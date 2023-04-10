49a36d6e3c08208b6ce3113d02e26bc364f515d3
Valtemir Junior <100030801+ValtemirJr@users.noreply.github.com>
2023-04-10 09:38:15 -0300

    Movimentos

---
+++ DesafioCarteira/Controllers/MovimentoController.cs
@@ -0,0 +1,45 @@
+﻿using DesafioCarteira.Models;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioCarteira.Controllers
+{
+    public class MovimentoController : Controller
+    {
+        private readonly ISession _session;
+
+        public MovimentoController(ISession session)
+        {
+            _session = session;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> NovoMovimento(int pessoaId)
+        {
+            return View(await _session.GetAsync<Pessoa>(pessoaId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NovoMovimento(MovimentoEntrada entrada)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                {
+                    await _session.SaveAsync(entrada);
+                    await transaction.CommitAsync();
+                    return RedirectToAction(nameof(NovoMovimento));
+                }
+            }
+            return View(entrada);
+        }
+    }
+}
--- DesafioCarteira/Controllers/PessoaController.cs
+++ DesafioCarteira/Controllers/PessoaController.cs
@@ -134,5 +134,7 @@ namespace DesafioCarteira.Controllers
             }
             return View(pessoa);
         }
+
+        
     }
 }
